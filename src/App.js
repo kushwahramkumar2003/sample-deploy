7bac285fa117af8603f309f766fbe609973eecb3
kushwahramkumar2003 <kushwahramkumar2003gmail.com>
2024-01-01 13:50:42 +0530

    Update text in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye Bye 2023!</p>
+        <p>Bye Bye 2023! and code 2024</p>
       </header>
     </div>
   );
